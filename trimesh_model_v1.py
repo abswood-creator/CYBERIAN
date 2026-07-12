655676dccaf0e4acdf426ae9c68a3bf8f8480999
tesi <abswood@icloud.com>
2026-07-12 16:45:48 -0600

    Update trimesh_model_v1.py

--- trimesh_model_v1.py
+++ trimesh_model_v1.py
@@ -1,1 +1,18 @@
+import trimesh
+import numpy as np
 
+vertices = np.array([
+    [0, 0, 0],
+    [0, 10, 0],
+    [5, 15, 0],
+    [5, 5, 0]
+])
+
+faces = np.array([
+    [0, 1, 2],
+    [0, 2, 3]
+])
+
+mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+
+# mesh.show()
