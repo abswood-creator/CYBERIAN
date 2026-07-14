bf1be4866aa91c7c7844670bbec337d798813d08
tesi <abswood@icloud.com>
2026-07-14 07:45:27 -0600

    Create cell_1.py

---
+++ Fits_View_High_Colors/cell_1.py
@@ -0,0 +1,6 @@
+# --- Cell 1: imports ---
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from astropy.io import fits
+from astropy.visualization import ZScaleInterval
